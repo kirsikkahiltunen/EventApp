f6092df02ea1f949f16baa5a64131f8b7a51a640
Kirsikka Hiltunen <kirsikka.hiltunen@gmail.com>
2024-05-05 23:54:51 +0300

    Modifications for sending messages.

--- users.py
+++ users.py
@@ -6,9 +6,10 @@ from sqlalchemy.sql import text
 import re
 import secrets
 
+
 def login(username, password):
     sql = text("SELECT* FROM users WHERE username=:username")
-    result = db.session.execute(sql, {"username":username})
+    result = db.session.execute(sql, {"username": username})
 
     user = result.fetchone()
 
@@ -24,60 +25,58 @@ def login(username, password):
         else:
             return False
 
+
 def logout():
     del session["username"]
     del session["user_id"]
+
 
 def register(username, password1):
     password_hash = generate_password_hash(password1)
     sql = text("""INSERT INTO users (username, password) 
                     VALUES (:username, :password)""")
-    db.session.execute(sql, {"username":username, "password":password_hash})
+    db.session.execute(sql, {"username": username, "password": password_hash})
     db.session.commit()
-        
+
     return login(username, password1)
 
+
 def valid_password(password):
-    special_characters= """!@#$%&*/()[]-_=+}?';,:.<>\``~^{"""
-    lower=0
-    upper=0
-    digit=0
-    special=0
+    special_characters = """!@#$%&*/()[]-_=+}?';,:.<>\``~^{"""
+    lower = 0
+    upper = 0
+    digit = 0
+    special = 0
     for i in password:
 
         if (i.islower()):
-            lower+=1
-        
+            lower += 1
+
         if (i.isupper()):
-            upper+=1
+            upper += 1
 
         if (i.isdigit()):
-            digit+=1
+            digit += 1
 
         if (i in special_characters):
-            special+=1
-    
-    print (f"lower={lower}, upper={upper}, digit={digit}, special={special}")
+            special += 1
 
-    if (lower>=1 and upper>=1 and digit>=1 and special>=1):
+    print(f"lower={lower}, upper={upper}, digit={digit}, special={special}")
+
+    if (lower >= 1 and upper >= 1 and digit >= 1 and special >= 1):
         return True
-    
+
     return False
-   
+
+
 def username_already_exists(username):
     sql = text("SELECT* FROM users WHERE username=:username")
-    result = db.session.execute(sql, {"username":username})
+    result = db.session.execute(sql, {"username": username})
     user = result.fetchone()
     if user:
         return True
     return False
 
+
 def user_id():
     return session.get("user_id", 0)
-
-
-
-
-        
-    
-        
